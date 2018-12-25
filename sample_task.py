619a1b6308274252945841e4fa22657c00a36657
dugu9sword <dugu9sword@163.com>
2018-12-25 19:39:45 +0800

    放到项目里优化一下先

--- sample_task.py
+++ sample_task.py
@@ -1,10 +1,29 @@
 import time
 import random
 import os
+import argparse
 
-print(os.environ["CUDA_VISIBLE_DEVICES"])
+
+class ProgramArgs(argparse.Namespace):
+    def __init__(self):
+        super(ProgramArgs, self).__init__()
+        self.max_span_length = 10
+        self.max_sentence_length = 120
+
+
+parser = argparse.ArgumentParser()
+nsp = ProgramArgs()
+for key, value in nsp.__dict__.items():
+    parser.add_argument('--{}'.format(key),
+                        action='store',
+                        default=value,
+                        type=type(value),
+                        dest=str(key))
+config = parser.parse_args(namespace=nsp)  # type: ProgramArgs
+
+print("arg a is {}, b is {}".format(config.a, config.b))
+print("cuda device is {}".format(os.environ["CUDA_VISIBLE_DEVICES"]))
 
 sec = random.randint(3, 10)
-print("A task to sleep for {} seconds".format(sec))
+print("sleep for {} seconds".format(sec))
 time.sleep(sec)
-
