20c590969c2207f44b240634abcc55af4a0b86f9
Yi <dugu9sword@163.com>
2021-02-20 10:47:20 +0000

    support extraction

--- bash_sample.sh
+++ bash_sample.sh
@@ -1,1 +1,7 @@
 echo "this is a bash sample, the args are" $1 $2
+echo 'epoch 1 score:' $RANDOM
+echo 'epoch 2 score:' $RANDOM
+echo 'epoch 3 score:' $RANDOM
+echo 'epoch 4 score:' $RANDOM
+echo 'epoch 5 score:' $RANDOM
+echo 'DONE'
