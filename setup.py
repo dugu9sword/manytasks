a26ee846328b094163e90ef6f713aa8cda6a42ff
zhouyi <dugu9sword@163.com>
2024-09-09 20:06:27 +0800

    version 2.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="manytasks",
-    version="2.2",
+    version="2.3",
     keywords=["manytasks", ],
     description="A tool for deploying many tasks automatically.",
     long_description=long_description,
